b3b0f98c94daf89b25860b6da23e33ed8f0f31b9
19550Manish <91590633+19550Manish@users.noreply.github.com>
2021-09-29 16:56:01 +0530

    Update AccountController.java

--- demo/src/main/java/com/example/demo/AccountController.java
+++ demo/src/main/java/com/example/demo/AccountController.java
@@ -8,7 +8,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RequestMapping("/Account")
 @RestController
 public class AccountController {
 	
@@ -17,12 +16,11 @@ public class AccountController {
 	String postCall(@RequestBody Account account) {
 		System.out.println("name " + account.getName());
 		System.out.println("city " + account.getCity());
-		//System.out.println("state " +account.getState());
-	//	System.out.println("pin " +account.getPin());
-//		System.out.println("BalanceAmount" +account.getBalanceAmount());
-//		System.out.println("Status " +account.getStatus());
-		return "post..";
+		System.out.println("state " +account.getState());
+		System.out.println("pin " +account.getPin());
+		System.out.println("BalanceAmount" +account.getBalanceAmount());
+		System.out.println("Status " +account.getStatus());
+		return "Account controller";
 	}
 	
-
 }
